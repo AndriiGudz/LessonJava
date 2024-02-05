597b42a4fb0bddd2ee51b83a3014ac71dc666342
AndriiGudz <andrey.gudz@gmail.com>
2024-02-05 21:13:19 +0100

    HW14 task0 - editing

--- src/homework_14/Task0.java
+++ src/homework_14/Task0.java
@@ -6,7 +6,12 @@ public class Task0 {
 //        toUpperCase() использовать низзя.
 
         String myName = "andRRey gudz 12 !@:";
+
+        System.out.println("Вариант 1");
         stringUpperCase(myName);
+
+        System.out.println("\nВариант 2");
+        System.out.println(stringToUpperCase(myName));
 
     }// End
 
@@ -18,6 +23,19 @@ public class Task0 {
             }
             System.out.print(simbol);
         }
+    }
+
+    public static String stringToUpperCase(String str1) {
+        String result = "";
+        for (int i = 0; i < str1.length(); i++) {
+            char simbol = str1.charAt(i);
+            if (str1.charAt(i) >= 97 & str1.charAt(i) <= 122) {
+                simbol -= 32;
+            }
+            result += simbol;
+        }
+//        System.out.println(result);
+        return result;
     }
 
 }
